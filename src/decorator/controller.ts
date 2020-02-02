c0f49a2814d12c0cef3a28211518f866662ef118
printjs <heaventone@yeah.net>
2020-02-02 16:23:07 +0800

    Modified params is optional

--- src/decorator/controller.ts
+++ src/decorator/controller.ts
@@ -1,8 +1,8 @@
 import { DecoratorKey } from "../constant/decorator.key";
 import { defineMetadata } from "../tool/reflect";
 
-export function Controller(path: string) {
-    return <T extends new(...args: any[]) => {}>(target: T) => {
-        defineMetadata(DecoratorKey.Controller, path, target);
+export function Controller(path?: string) {
+    return <T extends new (...args: any[]) => {}>(target: T) => {
+        defineMetadata(DecoratorKey.Controller, path || "", target);
     };
 }
